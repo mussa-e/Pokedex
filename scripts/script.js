efcc0f97dc744e534bdf1dea99932eafcce3f068
Mussa Faragi <mussa.faragi@gmx.de>
2025-06-27 21:51:57 +0200

    search user feedback added

--- scripts/script.js
+++ scripts/script.js
@@ -85,12 +85,19 @@ function search() {
     let container = document.getElementById('gallery');
     container.innerHTML = '';
 
+    let matchesFound = false;
+
     for (let i = 0; i < allPokemon.length; i++) {
         let name = allPokemon[i].results[i].name.toLowerCase();
 
         if (name.includes(searchValue)) {
             container.innerHTML += getPokTemplate(allPokemon[i], i, allPokemonData[i]);
+            matchesFound = true;
         }
+    }
+
+    if (!matchesFound) {
+        container.innerHTML = `<div class="search-user-feedback">No matches found</div>`;
     }
 }
 
